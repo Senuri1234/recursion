8f29fe9c1c3253adea2cb55f18929bb5a92a351e
Senuri1234 <158792381+Senuri1234@users.noreply.github.com>
2024-08-22 17:56:02 +0530

    Add files via upload

---
+++ Recursion/src/recursion.java
@@ -0,0 +1,23 @@
+
+public class recursion {
+	 // Recursive method to calculate factorial
+    public static int factorial(int n) {
+        // Base case: if n is 0 or 1, return 1
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        // Recursive case: n * factorial of (n-1)
+        return n * factorial(n - 1);
+    }
+
+    public static void main(String[] args) {
+        int number = 5; // You can change this value to test with other numbers
+        int result = factorial(number);
+        System.out.println("Factorial of " + number + " is: " + result);
+    }
+
+}
+
+
+   
+
